d241aca71687a020b112abd36b41b784ce0fdbd5
SytheSV <12101871@student.pxl.be>
2022-10-28 16:33:58 +0200

    updated navbar

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SyeSV-Website
 Github For Sye's Website
+
+
+Nav-bar inladen via JS onder window width condition. Als windows.width <= 756px / 900px navbar in column.
+
+Nav-bar inladen via JS onder window width condition. Als windows.width navbar in row.
--- Website/JS/Hamburger.js
+++ Website/JS/Hamburger.js
@@ -4,20 +4,13 @@ menuBtn.addEventListener('click',() =>{
     if (!menuOpen){
         menuBtn.classList.add('open');
         menuOpen = true;
-        // navLinks.style.display= 'none';
-        // navSocials.style.display= 'none';
-        // navContact.style.display= 'none';
     }
     else{
         menuBtn.classList.remove('open');
         menuOpen = false;
-        // navLinks.style.display= 'flex';
-        // navSocials.style.display= 'flex';
-        // navContact.style.display= 'flex';
     }
 });
+// let navLinks = document.querySelector('#nav-links-section');
+// let navSocials = document.querySelector('#nav-socials-section');
+// let navContact = document.querySelector('#nav-contact-section');//
 
-
-let navLinks = document.querySelector('#nav-links-section');
-let navSocials = document.querySelector('#nav-socials-section');
-let navContact = document.querySelector('#nav-contact-section');
