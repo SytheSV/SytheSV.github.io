e17f8154f8532da1b4a53c300aefbaa0689e9048
SytheSV <12101871@student.pxl.be>
2022-10-28 17:22:15 +0200

    Added responsiveness to navbar
    Working hamburger menu toggling navbar in small screen mode (less than 900px)

--- Website/JS/Hamburger.js
+++ Website/JS/Hamburger.js
@@ -3,14 +3,14 @@ let menuOpen = false;
 menuBtn.addEventListener('click',() =>{
     if (!menuOpen){
         menuBtn.classList.add('open');
+        navBottomBar.style.display = 'flex';
         menuOpen = true;
     }
     else{
         menuBtn.classList.remove('open');
         menuOpen = false;
+        navBottomBar.style.display = 'none';
     }
 });
-// let navLinks = document.querySelector('#nav-links-section');
-// let navSocials = document.querySelector('#nav-socials-section');
-// let navContact = document.querySelector('#nav-contact-section');//
+let navBottomBar = document.querySelector('#nav-bottom-bar');
 
