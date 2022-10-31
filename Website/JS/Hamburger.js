765e8c31ccccf172ad1210d2f38fc94b39ccff08
SytheSV <12101871@student.pxl.be>
2022-10-31 22:36:05 +0100

    Added a hover color

--- Website/JS/Hamburger.js
+++ Website/JS/Hamburger.js
@@ -1,15 +1,18 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
+let navBottomBar = document.querySelector('#nav-bottom-bar');
+let mainContent = document.querySelector('.mainContent');
 menuBtn.addEventListener('click',() =>{
     if (!menuOpen){
         menuBtn.classList.add('open');
         navBottomBar.style.display = 'flex';
         menuOpen = true;
+        mainContent.style.display = 'none';
     }
     else{
         menuBtn.classList.remove('open');
         menuOpen = false;
         navBottomBar.style.display = 'none';
+        mainContent.style.display = 'block';
     }
 });
-let navBottomBar = document.querySelector('#nav-bottom-bar');
