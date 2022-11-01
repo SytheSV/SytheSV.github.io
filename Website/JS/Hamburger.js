02c07f23293e70a6592e53568783efcdcf5155d0
SytheSV <12101871@student.pxl.be>
2022-11-01 23:30:43 +0100

    added page scroll

--- Website/JS/Hamburger.js
+++ Website/JS/Hamburger.js
@@ -2,17 +2,20 @@ let menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 let navBottomBar = document.querySelector('#nav-bottom-bar');
 let mainContent = document.querySelector('.mainContent');
+let sideBar = document.querySelector('.sidebar')
 menuBtn.addEventListener('click',() =>{
     if (!menuOpen){
         menuBtn.classList.add('open');
         navBottomBar.style.display = 'flex';
         menuOpen = true;
         mainContent.style.display = 'none';
+        sideBar.style.display = 'none'
     }
     else{
         menuBtn.classList.remove('open');
         menuOpen = false;
         navBottomBar.style.display = 'none';
         mainContent.style.display = 'block';
+        sideBar.style.display = 'flex'
     }
 });
